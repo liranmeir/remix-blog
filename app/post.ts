8882ea2934e7e87b9e927f7ef91ae1ef297206d3
liranmeir <liranmeir@gmail.com>
2022-02-26 00:46:04 +0200

    adding admin section

--- app/post.ts
+++ app/post.ts
@@ -8,6 +8,11 @@ export type Post = {
   slug: string;
   title: string;
 };
+type NewPost = {
+  title: string;
+  slug: string;
+  markdown: string;
+};
 export type PostMarkdownAttributes = {
   title: string;
 };
@@ -48,4 +53,11 @@ export async function getPost(slug: string) {
   );
   const html = marked(body);
   return { slug, html, title: attributes.title };
+}
+
+// ...
+export async function createPost(post: NewPost) {
+  const md = `---\ntitle: ${post.title}\n---\n\n${post.markdown}`;
+  await fs.writeFile(path.join(postsPath, post.slug + ".md"), md);
+  return getPost(post.slug);
 }
